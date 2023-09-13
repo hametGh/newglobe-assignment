13d0f5b79727ffef2b3f7dcde6be236cc7066caf
Hamed Valigholizadeh <hamet.gh@gmail.com>
2023-09-13 13:48:54 +0200

    :package: Webpack added

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './static/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'static'),
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
